14179dc0a39f1bc6e8210b75cbe17041330c701a
GalihEL <rb.mohgalih19@gmail.com>
2023-07-12 11:43:17 +0700

    Add description, images, and link column

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 with col1:
@@ -11,3 +11,21 @@ with col2:
     st.info(content)
 st.info("""In this portfolio i have created 20 apps which 5 of them provide a mutualism 
 relating big companies such as google, microsoft, AWS, etc.""")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
